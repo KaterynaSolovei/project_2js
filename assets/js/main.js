f2efdca10fef3cb5a1f8c6b08887f5cde7dd19d5
Kateryna Solovei <soloveikat07@gmail.com>
2023-03-26 22:47:22 -0400

    basic in process

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,9 +1,19 @@
 const slides = document.querySelectorAll('.slide');
-const indicatorsContainer = document.querySelector('#indicators-container')
+const indicatorsContainer = document.querySelector('#indicators-container');
 const indicatorItems = document.querySelectorAll('.indicator');
 const pauseBtn = document.querySelector('#pause-btn');
 const prevBtn = document.querySelector('#prev-btn');
 const nextBtn = document.querySelector('#next-btn');
+
+const SLIDES_LENGTH = slides.length;
+const CODE_ARROW_LEFT ='ArrowLeft';
+const CODE_ARROW_RIGHT ='ArrowRight';
+
+const CODE_SPACE ='Space';
+const FA_PAUSE = '<i class="fas fa-pause-circle"></i>';
+const FA_PLAY = '<i class="fas fa-play-circle"></i>';
+
+
 
 
 let currentSlide = 0;
@@ -13,7 +23,7 @@ let isPlaying = true;
 function gotoNth(n) {
   slides[currentSlide].classList.toggle('active');
   indicatorItems[currentSlide].classList.toggle('active')
-  currentSlide = (n + slides.length) % slides.length;
+  currentSlide = (n + SLIDES_LENGTH) % SLIDES_LENGTH;
   slides[currentSlide].classList.toggle('active');
   indicatorItems[currentSlide].classList.toggle('active')
 }
@@ -40,14 +50,14 @@ function pauseHandler() {
   if (isPlaying) {
     clearInterval(timerID);
     isPlaying = false;
-    pauseBtn.innerHTML = 'Play';
+    pauseBtn.innerHTML = FA_PLAY;
   }
 }
 
 function playHandler() {
   timerID = setInterval(gotoNext, 2000);
   isPlaying = true;
-  pauseBtn.innerHTML = 'Pause';
+  pauseBtn.innerHTML = FA_PAUSE;
 }
 
 const pausePlayHandler = () => isPlaying ? pauseHandler() : playHandler();
@@ -62,23 +72,17 @@ function indicate(e) {
   }
 }
 
+function pressKey(e) {
+  if(e.code === CODE_SPACE) pausePlayHandler();
+  if(e.code === CODE_ARROW_LEFT) prevHandler();
+  if(e.code === CODE_ARROW_RIGHT) nextHandler();
+
+}
+
 pauseBtn.addEventListener('click', pausePlayHandler);
 prevBtn.addEventListener('click', prevHandler);
 nextBtn.addEventListener('click', nextHandler);
 indicatorsContainer.addEventListener('click', indicate);
+document.addEventListener('keydown', pressKey)
 
 timerID = setInterval(gotoNext, 2000);
-
-/*
-function pauseHandler() {
-  if (isPlaying) {
-    clearInterval(timerID);
-    isPlaying = false;
-    pauseBtn.innerHTML = FA_PLAY;
-  } else {
-    timerID = setInterval(gotoNext, 2000);
-    isPlaying = true;
-    pauseBtn.innerHTML = FA_PAUSE;
-  };
-}
-*/
